d2d4b4acfab76ee13ab490dbda217299f6e52675
Steve <4391026+torvista@users.noreply.github.com>
2021-04-04 19:08:29 +0200

    fileset version 3.4 19/04/2019 from Zen Cart Plugins

---
+++ includes/classes/observers/class.google_recaptcha.php
@@ -0,0 +1,48 @@
+<?php
+/**
+*
+* @package plugins
+* @copyright Copyright 2003-2012 Zen Cart Development Team
+* @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+*
+* Designed for v1.5.6
+* Observer class for Google reCaptcha
+*/
+
+class google_recaptcha extends base {
+	/**
+	 * @var array
+	 */
+	private $pages_to_checkcheck = array();
+
+	function __construct() {
+
+		//comment out any pages you do NOT want to check
+		$pages_to_check[] =  'NOTIFY_CONTACT_US_CAPTCHA_CHECK';
+		$pages_to_check[] =  'NOTIFY_CREATE_ACCOUNT_CAPTCHA_CHECK';
+		$pages_to_check[] =  'NOTIFY_REVIEWS_WRITE_CAPTCHA_CHECK';
+		$this->attach($this,$pages_to_check);
+	}
+
+	function update(&$class, $eventID, $paramsArray = array()) {
+		global $messageStack, $error, $privatekey;
+
+		require_once __DIR__ . '/google/autoload.php';
+		//$recaptcha = new \ReCaptcha\ReCaptcha($privatekey);
+		// If file_get_contents() is locked down on your PHP installation to disallow
+		// its use with URLs, then you can use the alternative request method instead.
+		// This makes use of fsockopen() instead.
+		//$recaptcha = new \ReCaptcha\ReCaptcha($privatekey, new \ReCaptcha\RequestMethod\SocketPost());
+		// This makes use of curl instead
+		$recaptcha = new \ReCaptcha\ReCaptcha($privatekey, new \ReCaptcha\RequestMethod\Curl());
+
+		$resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+
+		if (!$resp->isSuccess()) {
+			$event_array = array('NOTIFY_CONTACT_US_CAPTCHA_CHECK' => 'contact', 'NOTIFY_CREATE_ACCOUNT_CAPTCHA_CHECK' => 'create_account', 'NOTIFY_REVIEWS_WRITE_CAPTCHA_CHECK' => 'review_text');
+			$messageStack->add($event_array[$eventID], $resp->getErrors());
+			$error = true;
+		}
+		return $error;
+	}
+}
---
+++ includes/languages/english/extra_definitions/reCaptcha.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package languageDefines
+ * @copyright Copyright 2003-2010 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: cardinal3dsecure.php 15757 2010-03-31 19:13:24Z drbyte $
+ */
+
+	define('RECAPTCHA_MISSING_INPUT_SECRET' , 'missing-input-secret');
+	define('RECAPTCHA_INVALID_INPUT_SECRET' , 'invalid-input-secret');
+	define('RECAPTCHA_MISSING_INPUT_RESPONSE' , 'Please verify that you are not a robot');
+	define('RECAPTCHA_INVALID_INPUT_RESPONSE' , 'Sorry, please verify again that you are not a robot');
